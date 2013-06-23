6a5326ffb6888ca8f973664a72e7b25ff94c3fb4
Louis Nyffenegger <louis.nyffenegger@gmail.com>
2013-06-23 17:32:11 +1100

    Fix SQL injection and XSS
    Just 2 more bugs,
    
    If you need any help let me know.

--- dina_meta1.php
+++ dina_meta1.php
@@ -1,9 +1,9 @@
 <?php
-$sql = mysql_query("select nama_produk from produk where id_produk='$_GET[id]'");
+$sql = mysql_query("select nama_produk from produk where id_produk='".intval($_GET[id])."'");
 $j   = mysql_fetch_array($sql);
 
 if (ISSET($_GET[id])){
-		echo "$j[nama_produk]";
+		echo htmlentities("$j[nama_produk]");
 }
 else{
 		echo ".::GriyaGaya - Ekspresikan Gaya Anda::.";
